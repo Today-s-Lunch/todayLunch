6aeb05153ace0e8d566eda1d163466fb21caca82
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-11-07 19:52:34 +0900

    underbar UI 및 기능 연결 완료
    클릭 시 색상 변경

--- app/src/main/java/com/example/todaylunch/RandomActivity.kt
+++ app/src/main/java/com/example/todaylunch/RandomActivity.kt
@@ -1,5 +1,6 @@
 package com.example.todaylunch
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
@@ -12,16 +13,33 @@ class RandomActivity : AppCompatActivity() {
     val binding: ActivityRandomBinding by lazy { ActivityRandomBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // 특정 단어만 색상 변경
+        // 점심 색상만 변경
         val text = "오늘의 점심은"
         val spannableString = SpannableString(text).apply {
             setSpan(ForegroundColorSpan(Color.parseColor("#FF6A50")), 4, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
         binding.Today.text = spannableString
+
+        //------------------------언더바 설정
+        // 버튼 클릭 리스너 설정
+        binding.underbar.backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed() // 이전 페이지로 이동
+        }
+
+        val goToStartActivity = Intent(this, StartActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+
+        binding.underbar.homeButton.setOnClickListener {
+            startActivity(goToStartActivity)
+        }
+
+        binding.underbar.myPageButton.setOnClickListener {
+            startActivity(goToStartActivity)
+        }
     }
 }
