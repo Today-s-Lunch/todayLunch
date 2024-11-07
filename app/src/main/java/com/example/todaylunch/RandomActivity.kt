7d592c4106f2c5f8bc714f4c8ade8fa87145a6f8
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-11-07 17:44:22 +0900

    random page UI 구성
    -랜덤 식당 추천 페이지 구성
    -underbar icon 추가
    -전체 배경 흰색으로 바꿈

--- app/src/main/java/com/example/todaylunch/RandomActivity.kt
+++ app/src/main/java/com/example/todaylunch/RandomActivity.kt
@@ -1,6 +1,10 @@
 package com.example.todaylunch
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todaylunch.databinding.ActivityRandomBinding
 
@@ -10,8 +14,14 @@ class RandomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar) //액션 바 연결(툴바)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // 특정 단어만 색상 변경
+        val text = "오늘의 점심은"
+        val spannableString = SpannableString(text).apply {
+            setSpan(ForegroundColorSpan(Color.parseColor("#FF6A50")), 4, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+        binding.Today.text = spannableString
     }
 }
