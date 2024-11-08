7ef034b01b98aa2db5b2ccd62b0e0331166eeeac
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-11-08 14:11:27 +0900

    랜덤 결과 이전 로딩창 추가

--- app/src/main/java/com/example/todaylunch/StartActivity.kt
+++ app/src/main/java/com/example/todaylunch/StartActivity.kt
@@ -1,35 +1,50 @@
 package com.example.todaylunch
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.os.Looper
+import android.os.Handler
 import android.view.GestureDetector
+import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
 import com.example.todaylunch.databinding.ActivityStartBinding
+import com.example.todaylunch.databinding.LoadingScreenBinding
 
 
 class StartActivity : AppCompatActivity() {
     private lateinit var gestureDetector: GestureDetector
+    private lateinit var loadingDialog: LoadingDialog
 
     lateinit var binding : ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        loadingDialog = LoadingDialog(this)
         binding.random.setOnClickListener {
-            val intent = Intent(this, RandomActivity::class.java)
-            startActivity(intent)
+            // 로딩 다이얼로그 표시
+            loadingDialog.show()
+
+            // 3초 후에 RandomActivity로 이동
+            Handler(Looper.getMainLooper()).postDelayed({
+                loadingDialog.dismiss() // 로딩 다이얼로그 닫기
+                val intent = Intent(this, RandomActivity::class.java)
+                startActivity(intent)
+            }, 3000) // 3초 지연
         }
         binding.school.setOnClickListener {
             val intent = Intent(this, SchoolActivity::class.java)
             startActivity(intent)
         }
 
-
+        // 언더바 설정
         val HOME = Intent(this, StartActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
         }
@@ -48,10 +63,24 @@ class StartActivity : AppCompatActivity() {
         binding.underbarMapinclude.myPageButton.setOnClickListener {
             startActivity(HOME)
         }
+    }
+}
 
+class LoadingDialog(context: Context) {
+    private val dialog: Dialog = Dialog(context)
+    private val binding: LoadingScreenBinding = LoadingScreenBinding.inflate(LayoutInflater.from(context))
 
+    init {
+        dialog.setContentView(binding.root)
+        dialog.setCancelable(false)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
     }
 
+    fun show() {
+        dialog.show()
+    }
 
-
+    fun dismiss() {
+        dialog.dismiss()
+    }
 }
