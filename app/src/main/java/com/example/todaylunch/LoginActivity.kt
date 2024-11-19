b17ca8f1b42d3b5c5ea3c2a428b276bb18a461ec
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-11-19 11:47:12 +0900

    Signup, Login xml 구성

---
+++ app/src/main/java/com/example/todaylunch/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.todaylunch
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todaylunch.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        // 점심 색상만 변경
+        val text = "오늘의 점심은"
+        val spannableString = SpannableString(text).apply {
+            setSpan(ForegroundColorSpan(Color.parseColor("#FF6A50")), 4, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+        binding.Today.text = spannableString
+
+        binding.goToSignup.setOnClickListener {
+            // SignUpActivity로 이동
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginBtn.setOnClickListener {
+            // SignUpActivity로 이동
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
