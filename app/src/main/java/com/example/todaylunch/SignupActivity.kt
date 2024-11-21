ba18f1ec74982009859b1433b28e420bd4b58981
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-11-21 11:13:27 +0900

    gradle 파일 추가 및 정리

--- app/src/main/java/com/example/todaylunch/SignupActivity.kt
+++ app/src/main/java/com/example/todaylunch/SignupActivity.kt
@@ -8,9 +8,14 @@ import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todaylunch.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class SignupActivity : AppCompatActivity() {
     val binding: ActivitySignupBinding by lazy { ActivitySignupBinding.inflate(layoutInflater) }
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
